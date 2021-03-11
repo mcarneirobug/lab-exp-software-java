8fd0ce41a454747a180fc20a46a62bcf2410e6cd
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-03-11 20:38:53 -0300

    refactor methods

--- Sprint 1 - Lab01S01/script.py
+++ Sprint 1 - Lab01S01/script.py
@@ -7,20 +7,22 @@ import os
 import os.path
 
 import shutil
+import stat
 from python_loc_counter import LOCCounter
 
 
-def run_github_query(query):
-    request = requests.post(f'{constant.URL}', json=query, headers=constant.HEADERS)
+# The function responsible for making requests in GitHub API
+def run_github_query(json):
+    request = requests.post(f'{constant.URL}', json=json, headers=constant.HEADERS)
     while request.status_code != 200:
         print("Error calling the API, processing again...")
         print(f'The query failed: {request.status_code}. {json["query"]}, {json["variables"]}')
         time.sleep(2)
-        request = requests.post(f'{constant.URL}', json=query, headers=constant.HEADERS)
+        request = requests.post(f'{constant.URL}', json=json, headers=constant.HEADERS)
     return request.json()
 
 
-# function responsible for saving GitHub repository urls in .txt file
+# The function responsible for saving GitHub repository urls in .txt file
 def export_urls_to_txt(urls_git_to_save_file):
     path = os.getcwd() + r"\urls_repos.txt"
     file = open(path, "w+")
@@ -28,9 +30,20 @@ def export_urls_to_txt(urls_git_to_save_file):
     file.close()
 
 
+def on_rm_error(func, path, exc_info):
+    # path contains the path of the file that couldn't be removed
+    # let's just assume that it's read-only and unlink it.
+    os.chmod(path, stat.S_IWRITE)
+    os.unlink(path)
+
+
 # The function delete an entire directory tree
 def clean_repository(path_folder):
-    shutil.rmtree(path_folder, ignore_errors=True)
+    try:
+        shutil.rmtree(path_folder, onerror=on_rm_error)
+    except Exception as e:
+        print(e)
+        print(f'\nThe directory {path_folder} cannot be deleted. Erase manually!')
 
 
 def clone_repository(git_path, directory_path):
@@ -45,7 +58,8 @@ def clone_repository(git_path, directory_path):
         print(f'\nTrying again...')
         clone_success = retry_clone_repository(git_path, directory_path)  # trying to clone again
         number_retries = 1
-        while not clone_success and number_retries <= 5:
+        retries = 10
+        while not clone_success and number_retries <= retries:
             clone_success = retry_clone_repository(git_path, directory_path)
             number_retries += 1
 
@@ -90,14 +104,16 @@ def export_to_csv(data):
 
         urls_git_to_save_file += node['url'] + '\n'  # to save urls at .txt file
 
-        repo_path = f'repository/java/{str(num_repositories)}'
+        repo_path = f'repository\\java\\{str(num_repositories)}'
         if os.path.exists(repo_path):
             clean_repository(repo_path)
+
+        num_repositories += 1  # incrementing to count next repository
 
         git_path = node['url'] + ".git"  # ex: https://github.com/mcarneirobug/lab-exp-software-java.git
         clone_repository(git_path, repo_path)
 
-        total_sloc = 0
+        total_src_loc = 0
         total_loc = 0
         total_blank_loc = 0
         total_comment_loc = 0
@@ -114,9 +130,9 @@ def export_to_csv(data):
                         print(f'Error to read file {full_path}. Trying read again...')
                         continue
 
-                    total_sloc += loc_data['source_loc']
-                    total_loc += loc_data['total_line_count']
-                    total_blank_loc += loc_data['blank_loc']
+                    total_src_loc += loc_data['source_loc']  # Everything that's not a comment or blank line
+                    total_loc += loc_data['total_line_count']  # Typical line count
+                    total_blank_loc += loc_data['blank_loc']  # Any whitespace designated only with string.whitespace
                     total_comment_loc += loc_data['total_comments_loc']
 
                     print(loc_data)
@@ -131,7 +147,7 @@ def export_to_csv(data):
                             + str(repository_age) + ";"
                             + str(node['stargazers']['totalCount']) + ";"
                             + str(node['releases']['totalCount']) + ";"
-                            + str(total_sloc) + ";"
+                            + str(total_src_loc) + ";"
                             + str(total_loc) + ";"
                             + str(total_blank_loc) + ";"
                             + str(total_comment_loc)
@@ -139,38 +155,41 @@ def export_to_csv(data):
     export_urls_to_txt(urls_git_to_save_file)
 
 
-# The flag is responsible for replacing the cursor on the next page
-finalQuery = constant.QUERY.replace("{AFTER}", "")
+def run():
+    # The flag is responsible for replacing the cursor on the next page
+    final_query = constant.QUERY.replace("{AFTER}", "")
 
-json = {
-    "query": finalQuery, "variables": {}
-}
+    json = {
+        "query": final_query, "variables": {}
+    }
 
-# Run application first time
-total_pages = 1
-print(f'Page -> {total_pages}')
-response = run_github_query(json)
-
-current_final_cursor = response["data"]["search"]["pageInfo"]["endCursor"]
-has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"]
-nodes = response["data"]["search"]["nodes"]
-
-# 5 repositories * 200 pages = 1000 repositories
-while total_pages < 3 and has_next_page:
-    total_pages += 1
+    # Run application first time
+    total_pages = 1
     print(f'Page -> {total_pages}')
-    next_query = constant.QUERY.replace("{AFTER}", ', after: "%s"' % current_final_cursor)
-    json["query"] = next_query
     response = run_github_query(json)
-    # Increase the output
-    nodes += response["data"]["search"]["nodes"]
-    # Changes to the next page
-    has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"]
-    # Changes the cursor to the current
-    current_final_cursor = response["data"]["search"]["pageInfo"]["endCursor"]
 
-if __name__ == "__main__":
+    current_final_cursor = response["data"]["search"]["pageInfo"]["endCursor"]
+    has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"]
+    nodes = response["data"]["search"]["nodes"]
+
+    # 5 repositories * 200 pages = 1000 repositories
+    while total_pages < 3 and has_next_page:
+        total_pages += 1
+        print(f'Page -> {total_pages}')
+        next_query = constant.QUERY.replace("{AFTER}", ', after: "%s"' % current_final_cursor)
+        json["query"] = next_query
+        response = run_github_query(json)
+        # Increase the output
+        nodes += response["data"]["search"]["nodes"]
+        # Changes to the next page
+        has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"]
+        # Changes the cursor to the current
+        current_final_cursor = response["data"]["search"]["pageInfo"]["endCursor"]
     csv_header()
     export_to_csv(nodes)
+
+
+if __name__ == "__main__":
+    run()
     print('#' * 100)
     print('\nSuccessfully generated csv file...')
